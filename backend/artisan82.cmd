5a535d65a97356f80e878e7d4c6f90f41779afbb
Rahul <rahul.giri@iotasol.com>
2026-07-08 15:52:25 +0530

    Backend delete

--- backend/artisan82.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-
-call "%~dp0php82.cmd" "%~dp0artisan" %*
-exit /b %ERRORLEVEL%
--- backend/composer82.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-
-call "%~dp0php82.cmd" "C:\composer\composer.phar" %*
-exit /b %ERRORLEVEL%
